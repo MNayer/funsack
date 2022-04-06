dcc0e2feb11f2dc30a20e893b40dfbe7d22cb9d4
Felix Weißberg <f.weissberg@tu-braunschweig.de>
2022-04-06 15:04:20 +0200

    Don't search for dwarf headers
    Expect dwarf headers to be located at (libdwarf/dwarf.h,
    libdwarf/libdwarf.h). If they're somewhere else, expect the user to
    create symlinks accordingly.

--- setup.py
+++ setup.py
@@ -1,43 +1,6 @@
 from setuptools import Extension, setup
 import os
 
-# Pre-setup adjustments
-# Search for libdwarf header files
-include_dirs = ["/usr/include", "/usr/local/include"]
-dwarf_paths = {
-    "libdwarf/libdwarf-0/libdwarf.h": "__libdwarf_libdwarf_0",
-    "libdwarf-0/libdwarf.h": "__libdwarf_0",
-    "libdwarf/libdwarf.h": "__libdwarf",
-}
-paths = (os.path.join(include_dir, dwarf_path) for include_dir in include_dirs for dwarf_path in dwarf_paths.keys())
-paths = filter(os.path.exists, paths)
-paths = list(paths)
-if len(paths) == 0:
-    raise Exception("Cannot find libdwarf header files.")
-if len(paths) > 1:
-    raise Exception("Too many libdwarf header files. Cannot decide which one to take.")
-
-# Find correct preprecessor directive
-header_path = paths[0]
-paths = map(lambda dwarf_path: (dwarf_path, len(dwarf_path)) if dwarf_path in header_path else (dwarf_path, 0), dwarf_paths)
-path,_ = sorted(paths, key=lambda el: el[1])[-1]
-directive = dwarf_paths[path]
-
-# Set preprecessor directive
-with open("dwarf.c", "r+") as f:
-    src = f.read()
-    f.seek(0)
-    f.write(f"#define {directive}\n")
-    f.write(src)
-
-# Begin setup
-ext_modules = [
-    Extension("funsack",
-              sources=["funsack.pyx"],
-              libraries=["dwarf"]
-              )
-]
-
 setup(
     name="FunSack",
     version="0.0.1",
